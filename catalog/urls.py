29d63f9d59353cf4fdf78c90f9af0797c8a82d82
Vlad.Burov <vburov1202@gmail.com>
2024-07-05 11:25:17 +0300

    first commit

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'catalog'
+
+urlpatterns = [
+    path('book_list/', views.book_list, name='book_list'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('create_category/', views.create_category, name='create_category')
+]
---
+++ catalog/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from .models import Book, Category
+from .forms import CategoryForm
+
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'catalog/book_list.html', {'books': books})
+
+
+def add_book(request):
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        published_date = request.POST.get('published_date')
+        category_id = request.POST.get('category')
+        category = get_object_or_404(Category, id=int(category_id))
+
+        try:
+            new_book = Book(
+                title=title,
+                author=author,
+                description=description,
+                price=price,
+                published_date=published_date,
+                category=category
+            )
+            new_book.full_clean()
+        except Exception as e:
+            return HttpResponse(f'Invalid values: {e}')
+        else:
+            new_book.save()
+            return redirect('catalog:book_list')
+
+    return render(request, 'catalog/add_book.html', {'categories': categories})
+
+
+def create_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('catalog:add_book')
+    else:
+        form = CategoryForm()
+
+    return render(request, 'catalog/create_category.html', {'form': form})
